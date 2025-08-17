09590a4ae55c5c170b0a82afabddae323347cb4d
Matías Hilú <matias.hilu@lmvsoftware.com>
2025-08-17 19:34:01 -0300

    GTM inline

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,6 @@ export const metadata: Metadata = {
     "msapplication-config": "/browserconfig.xml",
   },
 };
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-TCWT9RJD";
 export default function RootLayout({
   children,
 }: {
@@ -80,7 +79,7 @@ export default function RootLayout({
                   j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
               j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
               f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer',${GTM_ID});
+            })(window,document,'script','dataLayer','GTM-TCWT9RJD');
           `,
           }}
         />
@@ -91,7 +90,7 @@ export default function RootLayout({
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCWT9RJD}"
                       height="0" width="0"
                       style="display:none;visibility:hidden"></iframe>
             `,
