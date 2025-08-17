ddbd0dd6a81a4f964d4b5d6721cd54730519f57e
Matías Hilú <matias.hilu@lmvsoftware.com>
2025-08-17 18:42:41 -0300

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
@@ -37,14 +38,14 @@ export const metadata: Metadata = {
     "msapplication-config": "/browserconfig.xml",
   },
 };
-
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="en">
       <head>
         <link
           rel="apple-touch-icon"
@@ -68,18 +69,35 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         {/* <!-- Google Tag Manager --> */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TCWT9RJD');</script>
-{/* <!-- End Google Tag Manager --> */}
+        <Script
+          id="gtm-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer',${GTM_ID});
+          `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className={inter.className}>
         {/* <!-- Google Tag Manager (noscript) --> */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCWT9RJD"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-{/* <!-- End Google Tag Manager (noscript) --> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+                      height="0" width="0"
+                      style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {/* <CustomCursor /> */}
         <ScrollToTop />
         <Header />
